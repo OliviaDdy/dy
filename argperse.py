fad4b513bc7b83fc2003068e54132899ed18f7f7
OliviaDdy <790672784@qq.com>
2018-09-17 03:06:24 -0400

    arpagse

---
+++ argperse.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/9/17 下午2:48
+# @Author  : Dongyang
+# @File    : argperse.py
+# @Software: PyCharm
+
+import os, sys
+import numpy as np
+
+import argparse
+import os
+
+
+from util import util
+import torch
+
+class BaseOptions():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description='This is a PyMOTW sample program')
+        self.initialized = False
+
+    def initialize(self):
+        # experiment specifics
+        self.parser.add_argument('--name', type=str, default='label2city',
+                                 help='name of the experiment. It decides where to store samples and models')
+        # self.parser.add_argument('--gpu_ids', type=str, default='-1', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        self.parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
+        self.parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
+        self.parser.add_argument('--model', type=str, default='pix2pixHD', help='which model to use')
+        self.parser.add_argument('--norm', type=str, default='instance',
+                                 help='instance normalization or batch normalization')
+        self.parser.add_argument('--use_dropout', action='store_true', help='use dropout for the generator')
+        self.parser.add_argument('--data_type', default=32, type=int, choices=[8, 16, 32],
+                                 help="Supported data type i.e. 8, 16, 32 bit")
+        self.parser.add_argument('--verbose', action='store_true', default=False, help='toggles verbose')
+
+        self.initialized = True
+
+    def parse(self, save=True):
+        if not self.initialized:
+            self.initialize()
+        self.opt = self.parser.parse_args()
+        self.opt.isTrain = self.isTrain  # train or test
+
+        str_ids = self.opt.gpu_ids.split(',')
+        self.opt.gpu_ids = []
+        for str_id in str_ids:
+            id = int(str_id)
+            if id >= 0:
+                self.opt.gpu_ids.append(id)
+
+        # set gpu ids
+        if len(self.opt.gpu_ids) > 0:
+            print(self.opt.gpu_ids[0])
+            torch.cuda.set_device(self.opt.gpu_ids[0])
+
+        args = vars(self.opt)
+
+        print('------------ Options -------------')
+        for k, v in sorted(args.items()):
+            print('%s: %s' % (str(k), str(v)))
+        print('-------------- End ----------------')
+
+        # save to the disk
+        expr_dir = os.path.join(self.opt.checkpoints_dir, self.opt.name)
+        util.mkdirs(expr_dir)
+        if save and not self.opt.continue_train:
+            file_name = os.path.join(expr_dir, 'opt.txt')
+            with open(file_name, 'wt') as opt_file:
+                opt_file.write('------------ Options -------------\n')
+                for k, v in sorted(args.items()):
+                    opt_file.write('%s: %s\n' % (str(k), str(v)))
+                opt_file.write('-------------- End ----------------\n')
+        return self.opt
+
+
+class TrainOptions(BaseOptions):
+    def initialize(self):
+        BaseOptions.initialize(self)
+        # for displays
+        self.parser.add_argument('--display_freq', type=int, default=100,
+                                 help='frequency of showing training results on screen')
+        self.parser.add_argument('--print_freq', type=int, default=100,
+                                 help='frequency of showing training results on console')
+        self.parser.add_argument('--save_latest_freq', type=int, default=1000,
+                                 help='frequency of saving the latest results')
+        self.parser.add_argument('--save_epoch_freq', type=int, default=10,
+                                 help='frequency of saving checkpoints at the end of epochs')
+        self.parser.add_argument('--no_html', action='store_true',
+                                 help='do not save intermediate training results to [opt.checkpoints_dir]/[opt.name]/web/')
+        self.parser.add_argument('--debug', action='store_true',
+                                 help='only do one epoch and displays at each iteration')
+        # for training
+        self.parser.add_argument('--continue_train', action='store_true',
+                                 help='continue training: load the latest model')
+        self.isTrain = True
+
+
+opt = TrainOptions().parse()
